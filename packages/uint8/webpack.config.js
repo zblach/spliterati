cde79cb82e20d2867290a6fe1de6306c22e816ea
Zak Blacher <zblach@The-Cloud.local>
2021-01-27 12:01:57 -0800

    Added webpack.config.js

---
+++ packages/uint8/webpack.config.js
@@ -0,0 +1,16 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  entry: './src/uint8.ts',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }],
+  },
+};
